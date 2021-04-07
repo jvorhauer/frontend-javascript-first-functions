86207e813b00c091dcef2d080555a393b3b97c29
Jurjen Vorhauer <jurjen.vorhauer@jdriven.com>
2021-04-07 16:59:33 +0200

    Klaar met die functies.

--- functions-with-parameters.js
+++ functions-with-parameters.js
@@ -7,7 +7,6 @@
 function add(a, b) {
   return a + b;
 }
-
 const result = add(1, 4);
 console.log(result);
 
@@ -17,16 +16,26 @@ console.log(result);
 // greeter("Jan") geeft "Hoi Jan!"
 // greeter("Kees") geeft "Hoi Kees!"
 
-
+function greeter(name) {
+  return "Hoi " + name + "!";
+}
+const greeting = greeter("Jan");
+console.log(greeting);
+console.log(greeter("Kees"));
 
 /* Opdracht 2 */
 // Schrijf een functie genaamd minutesToSeconds, die een hoeveelheid minuten verwacht (als een getal) en teruggeeft hoeveel seconden dat zijn.
 // ---- Verwachte uitkomsten:
 // minutesToSeconds(1) geeft 60
 // minutesToSeconds(3) geeft 180
-// minutesToSeconds(23) geeft 1840
+// minutesToSeconds(23) geeft 1380
 
-
+function minutesToSeconds(minutes) {
+  return minutes * 60;
+}
+console.log("minutesToSeconds(1) geeft", minutesToSeconds(1), "seconden");
+console.log("minutesToSeconds(3) geeft", minutesToSeconds(3), "seconden");
+console.log("minutesToSeconds(23) geeft", minutesToSeconds(23), "seconden");
 
 /* Opdracht 3 */
 // Schrijf een functie genaamd merge, die twee strings verwacht en deze aan elkaar geplakt teruggeeft.
@@ -35,8 +44,13 @@ console.log(result);
 // merge("abra", "cadabra") geeft "abracadabra"
 // merge("zoet", "sappig") geeft "zoetsappig"
 
-
-
+function merge(s1, s2) {
+  return s1 + s2;
+}
+const abracadabra = merge("abra", "cadabra");
+console.assert(abracadabra === "abracadabra", "merged woord is %s", abracadabra);
+const zoetsappig = merge("zoet", "sappig");
+console.assert(zoetsappig === "zoetsappig", "het woord is %", zoetsappig);
 
 /* Opdracht  4 */
 // Schrijf een functie genaamd calculateDogYears die de hondenleeftijd verwacht en dit omrekent naar mensenjaren (1 tot 7). Op basis daarvan wordt een zinnetje teruggegeven.
@@ -44,7 +58,13 @@ console.log(result);
 // calculateDogYears(6) geeft "Jouw hond is 42 jaar oud in mensenjaren."
 // calculateDogYears(2) geeft "Jouw hond is 14 jaar oud in mensenjaren."
 
-
+function calculateHoomanYears(dogYears) {
+  return 7 * dogYears;
+}
+const hoomanYear6 = calculateHoomanYears(6);
+console.assert(hoomanYear6 === 42, "dog years zou 42 moeten zijn, maar is %d", hoomanYear6);
+const hoomanYear2 = calculateHoomanYears(2);
+console.assert(hoomanYear2 === 14, "dog years zou 14 moeten zijn, maar is %d", hoomanYear6);
 
 /* Opdracht 5 */
 // Schrijf een functie genaamd wrapper, die twee parameters verwacht: een woord en een karakter
@@ -54,6 +74,10 @@ console.log(result);
 // wrapper("beep", "_") geeft "_beep_"
 // wrapper("kaas", "Q") geeft "QkaasQ"
 
+function wrap(s, prePost) {
+  return prePost + s + prePost;
+}
+console.assert(wrap("bril", "*") === "*bril*");
 
 
 /* Bonus opdracht  */
@@ -62,4 +86,10 @@ console.log(result);
 // createDetailString({ firstName: 'Jan', lastName: 'Jansen', profession: 'docent'}) geeft "Het beroep vam Jan Jansen is docent."
 // createDetailString({ firstName: 'Kees', lastName: 'Klaasen', profession: 'brandweerman'}) geeft "Het beroep vam Kees Klaasen is brandweerman."
 
-
+function createDetailString(obj) {
+  return "Het beroep van " + obj.firstName + " " + obj.lastName + " is " + obj.profession;
+}
+const janJansen = createDetailString({ firstName: 'Jan', lastName: 'Jansen', profession: 'docent'});
+console.assert(janJansen === "Het beroep van Jan Jansen is docent", "'%s' werd niet verwacht", janJansen);
+const keesKlaasen = createDetailString({ firstName: 'Kees', lastName: 'Klaasen', profession: 'brandweerman'});
+console.assert(keesKlaasen === "Het beroep van Kees Klaasen is brandweerman", "'%s' werd neit verwacht", keesKlaasen);
--- functions.js
+++ functions.js
@@ -14,20 +14,48 @@ console.log(greeting);
 // Schrijf een functie genaamd getSchool, die de string "Novi Hogeschool" teruggeeft
 // ---- Verwachte uitkomst bij het aanroepen van de functie: "Novi Hogeschool"
 
-
+function getSchool() {
+  return "Novi Hogeschool";
+}
+console.assert(getSchool() === "Novi Hogeschool", "verwachte uitkomst getSchool borken");
 
 /* Opdracht 2 */
 // Schrijf een functie genaamd compliment, die jou een complimentje geeft!
 // ---- Verwachte uitkomst bij het aanroepen van de functie: "Lekker bezig met die functies, Jan!"
 
+function compliment() {
+  console.log("Lekker bezig met die functies, Jur!");
+}
+compliment();
 
 
 /* Opdracht 3 */
 // Schrijf een functie genaamd getGrades, die een array met de cijfers 8, 8.5, 6 en 7 teruggeeft.
 // ---- Verwachte uitkomst bij het aanroepen van de functie: [8, 8.5, 6, 7]
 
+function equalArrays(a1, a2) {
+  if (a1.length !== a2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < a1.length; i++) {
+      if (a1[i] !== a2[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
 
+function grades() {
+  return [8, 8.5, 6, 7];
+}
+console.assert(equalArrays(grades(), [8, 8.5, 6, 7]), "grades array is not correct");
 
 /* Opdracht 4 */
 // Schrijf een functie genaamd getDetails, die een object met daarin jouw eigen naam en achternaam teruggeeft.
 // ---- Verwachte uitkomst bij het aanroepen van de functie: { firstName: 'Jan', lastName: 'Janssen' }
+
+function getDetails() {
+  return {firstName: "Jurjen", lastName: "Vorhauer"};
+}
+console.assert(getDetails().firstName === "Jurjen" && getDetails().lastName === "Vorhauer", "object nie goe");
